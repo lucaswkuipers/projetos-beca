fbae97995e10617b5f17a4b73d2db4405d8041d1
Lucas Werner Kuipers <lucaswkuipers@gmail.com>
2021-03-17 21:38:40 -0300

    :sparkles: Nova tela de pesquisa de pacotes

---
+++ lucas-alura-viagens/lucas-alura-viagens/Pacotes/Controller/PacotesViagensViewController.swift
@@ -0,0 +1,74 @@
+//
+//  PacotesViagensViewController.swift
+//  lucas-alura-viagens
+//
+//  Created by Lucas Werner Kuipers on 17/03/2021.
+//  Copyright © 2021 everis. All rights reserved.
+//
+
+import UIKit
+
+class PacotesViagensViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
+
+    @IBOutlet weak var coloecaoPacotesViagem: UICollectionView!
+    @IBOutlet weak var pesquisarViagens: UISearchBar!
+    @IBOutlet weak var labelContadorPacotes: UILabel!
+    
+    let listaComTodasViagens: [Viagem] = ViagemDAO().retornaTodasAsViagens()
+    var listaViagens: [Viagem] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        listaViagens = listaComTodasViagens
+        
+        coloecaoPacotesViagem.dataSource = self
+        coloecaoPacotesViagem.delegate = self
+        pesquisarViagens.delegate = self
+        
+        self.labelContadorPacotes.text = atualizaContadorLabel()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.listaViagens.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let celulaPacote = collectionView.dequeueReusableCell(withReuseIdentifier: "celulaPacote", for: indexPath) as! PacoteViagemCollectionViewCell
+        
+        let viagemAtual = listaViagens[indexPath.item]
+        
+        celulaPacote.labelTitulo.text  = viagemAtual.titulo
+        celulaPacote.labelQuantidadeDias.text = "\(viagemAtual.quantidadeDeDias) dias"
+        celulaPacote.labelPreco.text = "R$ \(viagemAtual.preco)"
+        celulaPacote.imagemViagem.image = UIImage(named: viagemAtual.caminhoDaImagem)
+        
+        celulaPacote.layer.borderWidth = 0.5
+        celulaPacote.layer.borderColor = UIColor(red: 85.0/255.0, green: 85.0/255.0, blue: 85.0/255.0, alpha: 1).cgColor
+        celulaPacote.layer.cornerRadius = 8
+        
+        return celulaPacote
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let larguraCelula = collectionView.bounds.width / 2
+        return CGSize(width: larguraCelula - 10, height: 160)
+    }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        listaViagens = listaComTodasViagens
+        
+        if searchText != "" {
+            let filtroListaViagem = NSPredicate(format: "titulo contains %@", searchText)
+            let listaFiltrada: Array<Viagem> = (listaViagens as NSArray).filtered(using: filtroListaViagem) as! Array
+            listaViagens = listaFiltrada
+        }
+        self.labelContadorPacotes.text = self.atualizaContadorLabel()
+        coloecaoPacotesViagem.reloadData()
+    }
+    
+    func atualizaContadorLabel() -> String {
+        return listaViagens.count == 1 ? "1 pacote encontrado" : "\(listaViagens.count) pacotes encontrados"
+    }
+    
+}
---
+++ lucas-alura-viagens/lucas-alura-viagens/Pacotes/View/PacoteViagemCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  PacoteViagemCollectionViewCell.swift
+//  lucas-alura-viagens
+//
+//  Created by Lucas Werner Kuipers on 17/03/2021.
+//  Copyright © 2021 everis. All rights reserved.
+//
+
+import UIKit
+
+class PacoteViagemCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imagemViagem: UIImageView!
+    @IBOutlet weak var labelTitulo: UILabel!
+    @IBOutlet weak var labelQuantidadeDias: UILabel!
+    @IBOutlet weak var labelPreco: UILabel!
+    
+}
